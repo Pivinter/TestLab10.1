10128ebefa0639389627362084df9a298752bcd8
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-11-16 18:51:29 +0200

    Add project files.

---
+++ TestLab10.1/main.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* str, int i)
+{
+	if (str[i] != '\0')
+		if (str[i] == 'B' || str[i] == 'A' || str[i] == 'S' || str[i] == 'I' || str[i] == 'C')
+			return 1 + Count(str, i + 1);
+		else
+			return Count(str, i + 1);
+	else
+		return 0;
+}
+int main()
+{
+	char str[10];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 9);
+	cout << "String contained " << Count(str, 0) << " groups of 'BASIC'" << endl;
+	char* dest1 = new char[151];
+	dest1[0] = '\0';
+	cout << "Modified string (param) : " << dest1 << endl;
+	return 0;
+}
---
+++ UnitTest101/UnitTest101.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../TestLab10.1/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest101
+{
+	TEST_CLASS(UnitTest101)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = main();
+			Assert::AreEqual(t, 0);
+		}
+	};
+}
